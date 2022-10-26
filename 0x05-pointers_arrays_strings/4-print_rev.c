288173f84fe5409842aa9c38749373dd352fcc50
Gloria Ekene <gloriaekene11@gmail.com>
2022-10-26 04:03:40 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string to reverse
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if
+			(s[count] == '\n')
+		break;
+		count++;
+	}
+
+	for (count--; count >= 0; count--)
+		_putchar
+			(s[count])
+		_putchar('\n')
+}
