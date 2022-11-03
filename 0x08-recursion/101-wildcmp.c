49437a7958f51a9584900e30135e1ddc588f3999
Gloria Ekene <gloriaekene11@gmail.com>
2022-11-03 15:39:23 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings using recursion 
- * @s1: base string 
- * s2: string to compare 
- * Return: 1 if both are identical else 0
+ * wildcmp - compares two stringa using recursion
+ * @s1: base string
+ * @s2: string to compare
+ * Return: 1 if both are identical else 0
  */
 
 int wildcmp(char *s1, char *s2)
@@ -15,11 +15,9 @@ int wildcmp(char *s1, char *s2)
 			return (wildcmp(s1, s2 + 1));
 		return (*s2 == '\0');
 	}
-	
 	if (*s2 == '*')
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	else if (*s2 == *s1)
-		return (wildcmp(s1 + 1, s2 + 1));
-	
+		return (wildcmp(s1, s2 + 1));
 	return (0);
 }
